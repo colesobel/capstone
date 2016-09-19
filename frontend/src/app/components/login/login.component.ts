b01496259737639f10e4699d47f84c7ad3220518
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-19 11:17:51 -0600

    light form validation on login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -9,6 +9,26 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  login(username, password) {
+    let loginInfo = {}
+    loginInfo['username'] = username.value
+    loginInfo['password'] = password.value
+    console.log(loginInfo)
+  } 
+
+  signUp(firstName, lastName, username, password, passwordConfirmation) {
+    if (password.value !== passwordConfirmation.value) {
+      console.log('passwords must match');
+      return
+    }
+    let signupInfo = {}
+    signupInfo['firstName'] = firstName.value
+    signupInfo['lastName'] = lastName.value
+    signupInfo['username'] = username.value
+    signupInfo['password'] = password.value
+    console.log(signupInfo);
+  }
+
   ngOnInit() {
   }
 
