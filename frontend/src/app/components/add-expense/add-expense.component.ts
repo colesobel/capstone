4ca7326e1260406e033defb8588b359f7833ab29
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-19 07:29:03 -0600

    styling add expense page

--- frontend/src/app/components/add-expense/add-expense.component.ts
+++ frontend/src/app/components/add-expense/add-expense.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddExpenseComponent implements OnInit {
 
+  expenses: string[] = ['expense']
+  times: string[] = [
+    '12 AM',
+    '1 AM',
+    '2 AM',
+    '3 AM',
+    '4 AM',
+    '5 AM',
+    '6 AM',
+    '7 AM',
+    '8 AM',
+    '9 AM',
+    '10 AM',
+    '11 AM',
+    '12 PM',
+    '1 PM',
+    '2 PM',
+    '3 PM',
+    '4 PM',
+    '5 PM',
+    '6 PM',
+    '7 PM',
+    '8 PM',
+    '9 PM',
+    '10 PM',
+    '11 PM'
+  ]
+
+  dateString = new Date().toISOString().substring(0, 10)
+
+  addExpense() {
+    this.expenses.push('expense')
+    
+  }
+
   constructor() { }
 
   ngOnInit() {
