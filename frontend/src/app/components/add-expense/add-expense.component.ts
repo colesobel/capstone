efee136c79413da9dff14fb3bbbd94259537757d
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-19 10:52:39 -0600

    login page with some styling

--- frontend/src/app/components/add-expense/add-expense.component.ts
+++ frontend/src/app/components/add-expense/add-expense.component.ts
@@ -46,7 +46,6 @@ export class AddExpenseComponent implements OnInit {
     let expenseItems = document.getElementsByClassName('expense-container')
     let expenseObj = {}
     for(let i = 0; i < expenseItems.length; i++) {
-      console.log('hello')
       expenseObj[i] = {}
       expenseObj[i].expenseCategory = expenseItems[i].children[1].value
       expenseObj[i].amount = expenseItems[i].children[2].value
