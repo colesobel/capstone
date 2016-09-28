71de60dee3d8b5a2fcf2ea28799a6cfd43ad7ba0
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-28 00:03:52 -0600

    gauge values set from parent

--- frontend/src/app/components/gauge/gauge.component.ts
+++ frontend/src/app/components/gauge/gauge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gauge',
@@ -9,33 +9,21 @@ export class GaugeComponent implements OnInit {
 
   constructor() { }
 
+  @Input() gauge: any[];
+  @Input() options: any
+
+  drawChart = () => {
+    var data = google.visualization.arrayToDataTable(this.gauge);
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+    chart.draw(data, this.options);
+  }
+
   ngOnInit() {
 
     google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Memory', 80],
-          ['CPU', 55],
-          ['Network', 68], 
-          ['What the fuck', 100]
-        ]);
-
-        var options = {
-          width: 600, height: 200,
-          redFrom: 90, redTo: 100,
-          yellowFrom:75, yellowTo: 90,
-          minorTicks: 5
-        };
-
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
-
-        chart.draw(data, options);
-      }
-
-
+    google.charts.setOnLoadCallback(this.drawChart);
 
   }
 
