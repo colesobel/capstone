115eaeae58d53687b2007f35ee21afde2d87d27b
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-30 12:14:31 -0600

    fixed expenses returning with gauge stats

--- backend/models/dailyExpensesService.js
+++ backend/models/dailyExpensesService.js
@@ -20,12 +20,14 @@ let dailyExpensesService = {
             round(ec.percentage / 100::float * (select sum(expense_amount) from daily_expenses where user_id = ${user_id} and month = 'September')) as desired_spend_total,
             coalesce(sum(de.expense_amount), 0) as spend_total, 
             ec.percentage * 2 as gauge_max,
-            ui.monthly_income
+            ui.monthly_income,
+            coalesce(fe.expense_amount, 0) as fixed_expense_amount
             from expense_categories ec
             left join (select * from daily_expenses where user_id = ${user_id} and month = 'September') de on ec.user_id = de.user_id and ec.expense_category = de.expense_category
             left join (select user_id, monthly_income from user_income where user_id = ${user_id}) ui on ec.user_id = ui.user_id
+            left join (select user_id, expense_category, expense_amount from fixed_expenses where user_id = 6) fe on ec.user_id = fe.user_id and ec.expense_category = fe.expense_category
             where ec.user_id = ${user_id}
-            group by ec.user_id, ec.expense_category, ec.percentage, ui.monthly_income`).then(gaugeStats => resolve(gaugeStats.rows))
+            group by ec.user_id, ec.expense_category, ec.percentage, ui.monthly_income, fixed_expense_amount`).then(gaugeStats => resolve(gaugeStats.rows))
         })
     }
 }
